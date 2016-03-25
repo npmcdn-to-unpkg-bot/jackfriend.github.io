f05b132a2a71d65340bbfe025a9ddaf3b8e32b78
jackfriend <jackwillamfriend@gmail.com>
2016-03-24 21:51:13 -0400

    latest changes

--- assets/functions.js
+++ assets/functions.js
@@ -18,42 +18,15 @@ if (    $(window).width()    >     320) {
 var insultArray = [
   'People like you are the reason we have middle fingers.',
   'Mirrors can’t talk, lucky for you they can’t laugh either.',
-  'Why Don’t You Slip Into Something More Comfortable <i class="em em-wink"></i> Like A Coma?',
+  'Why Don\'t You Slip Into Something More Comfortable <i class="em em-wink"></i> Like A Coma?',
   'Tell me… Is being stupid a profession or are you just gifted?',
   'No need for insults, your face says it all.',
   'It’s not that you are weird… it’s just that everyone else is normal.',
   'You know most days when I look in a mirror I feel ugly, but when I look at you I feel lucky'
 ];
 
-var replyArray = [
-  'I\'m Sorry <i class="em em-sleepy"></i>',
-  'Haha <i class="em em-stuck_out_tongue_closed_eyes"></i>',
-  'No! Don\'t click me!'
-]
-
 var insult = insultArray[Math.floor(Math.random() * insultArray.length)];
-var reply = replyArray[Math.floor(Math.random() * replyArray.length)];
-
 $('.insult').append(insult);
-$('.reply').append(reply);
-
-if (    $(window).width()    < 600) {
-  $('.insult').empty();
-  $('.reply').empty();
-};
-
-$(window).resize(function () {
-
-  if (    $(window).width()    <= 600) {ulp
-
-    $('.insult').empty();
-    $('.reply').empty();
-  } else {
-    $('.insult').append(insult);
-    $('.reply').append(reply);
-  }
-
-});
 
 if (    $(window).width()    <= 600) {
   $('.nav-menu').hide();
