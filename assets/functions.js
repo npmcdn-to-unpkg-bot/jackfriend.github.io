c96a65610f87398a73cac9aa4cfbbde367e073b3
jackfriend <jackwillamfriend@gmail.com>
2016-04-24 12:25:10 -0400

    seo

--- assets/functions.js
+++ assets/functions.js
@@ -32,17 +32,6 @@ $('.fa-times').on('click', function () {
     $('.form').fadeOut('fast');
 });
 
-function initMap() {
-    var map = new google.maps.Map($('#map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
-        scrollwheel: false,
-        zoom: 8
-    });
-};
-
 if (    $(window).width()    <= 600) {
   $('.nav-menu').hide();
   $('.social-media').hide();
